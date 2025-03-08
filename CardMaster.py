c4bf5415895a4d7fc9b4ce8b22a37ed0aaa2abc9
Hiake <hiakemason@outlook.com>
2025-03-08 08:05:55 +0800

    launch.json some bugs

--- CardMaster.py
+++ CardMaster.py
@@ -120,15 +120,17 @@ class CardGameGUI:
         tk.Button(self.root, text="玩家1：比较两张卡牌之和", command=lambda: self.compare_sums(self.player1, self.player2)).pack()
 
         tk.Button(self.root, text="退出游戏", command=lambda: quit_game()).pack()
+
     def setup_game_screen_p2(self):
         self.clear_screen()
-        
+
         tk.Label(self.root, text="游戏进行中").pack()
 
         tk.Button(self.root, text="玩家2：比较单张卡牌", command=lambda: self.compare_cards(self.player2, self.player1)).pack()
         tk.Button(self.root, text="玩家2：比较两张卡牌之和", command=lambda: self.compare_sums(self.player2, self.player1)).pack()
 
         tk.Button(self.root, text="退出游戏", command=lambda: quit_game()).pack()
+
     def compare_cards(self, player1, player2):
         self.clear_screen()
 
@@ -137,14 +139,14 @@ class CardGameGUI:
         frame = tk.Frame(self.root)
         frame.pack()
         for card in player1.cards.keys():
-            tk.Radiobutton(frame, text=card, variable=self.card1_var, value=card).pack(side=tk.LEFT)
+            tk.Radiobutton(frame, text=card, variable=self.card1_var, value=card).pack()
 
         tk.Label(self.root, text=f"{player1.name}，请选择对方的卡牌：").pack()
         self.card2_var = tk.StringVar()
         frame = tk.Frame(self.root)
         frame.pack()
         for card in player2.cards.keys():
-            tk.Radiobutton(frame, text=card, variable=self.card2_var, value=card).pack(side=tk.LEFT)
+            tk.Radiobutton(frame, text=card, variable=self.card2_var, value=card).pack()
 
         tk.Button(self.root, text="确认比较", command=lambda: self.show_card_comparison(player1, player2)).pack()
 
@@ -163,7 +165,7 @@ class CardGameGUI:
             result = f"{card1}等于{card2}"
 
         messagebox.showinfo("比较结果", result)
-        
+
         match self.flag:
             case 1:
                 self.setup_game_screen_p2()
@@ -172,35 +174,54 @@ class CardGameGUI:
                 self.setup_game_screen_p1()
                 self.flag = 1
 
-
     def compare_sums(self, player1, player2):
         self.clear_screen()
+        self.step = 1  # 初始化步骤
+        self.selected_cards = []  # 存储选择的卡牌
 
-        tk.Label(self.root, text=f"{player1.name}，请选择你的两张卡牌：").pack()
-        self.card1_var = tk.StringVar()
-        self.card2_var = tk.StringVar()
-        frame = tk.Frame(self.root)
-        frame.pack()
-        for card in player1.cards.keys():
-            tk.Radiobutton(frame, text=card, variable=self.card1_var, value=card).pack(side=tk.LEFT)
-            tk.Radiobutton(frame, text=card, variable=self.card2_var, value=card).pack(side=tk.LEFT)
+        self.show_sum_selection(player1, player2)
 
-        tk.Label(self.root, text=f"{player1.name}，请选择对方的两张卡牌：").pack()
-        self.card3_var = tk.StringVar()
-        self.card4_var = tk.StringVar()
-        frame = tk.Frame(self.root)
-        frame.pack()
-        for card in player2.cards.keys():
-            tk.Radiobutton(frame, text=card, variable=self.card3_var, value=card).pack(side=tk.LEFT)
-            tk.Radiobutton(frame, text=card, variable=self.card4_var, value=card).pack(side=tk.LEFT)
+    def show_sum_selection(self, player1, player2):
+        if self.step == 1:
+            # 第一步：选择自己的两张牌
+            tk.Label(self.root, text=f"{player1.name}，请选择你的两张卡牌：").pack()
+            self.card1_var = tk.StringVar()
+            self.card2_var = tk.StringVar()
+            frame = tk.Frame(self.root)
+            frame.pack()
+            for card in player1.cards.keys():
+                tk.Radiobutton(frame, text=card, variable=self.card1_var, value=card).pack(side=tk.LEFT)
+                tk.Radiobutton(frame, text=card, variable=self.card2_var, value=card).pack(side=tk.LEFT)
 
-        tk.Button(self.root, text="确认比较", command=lambda: self.show_sum_comparison(player1, player2)).pack()
+            tk.Button(self.root, text="下一步", command=lambda: self.next_step(player1, player2)).pack()
+        elif self.step == 2:
+            # 第二步：选择对方的两张牌
+            tk.Label(self.root, text=f"{player1.name}，请选择对方的两张卡牌：").pack()
+            self.card3_var = tk.StringVar()
+            self.card4_var = tk.StringVar()
+            frame = tk.Frame(self.root)
+            frame.pack()
+            for card in player2.cards.keys():
+                tk.Radiobutton(frame, text=card, variable=self.card3_var, value=card).pack(side=tk.LEFT)
+                tk.Radiobutton(frame, text=card, variable=self.card4_var, value=card).pack(side=tk.LEFT)
+
+            tk.Button(self.root, text="确认比较", command=lambda: self.show_sum_comparison(player1, player2)).pack()
+
+    def next_step(self, player1, player2):
+        # 存储玩家1选择的两张牌
+        self.selected_cards.append(self.card1_var.get())
+        self.selected_cards.append(self.card2_var.get())
+
+        self.step = 2  # 进入第二步
+        self.clear_screen()
+        self.show_sum_selection(player1, player2)
 
     def show_sum_comparison(self, player1, player2):
-        card1 = self.card1_var.get()
-        card2 = self.card2_var.get()
-        card3 = self.card3_var.get()
-        card4 = self.card4_var.get()
+        # 存储玩家2选择的两张牌
+        self.selected_cards.append(self.card3_var.get())
+        self.selected_cards.append(self.card4_var.get())
+
+        card1, card2, card3, card4 = self.selected_cards
 
         sum1 = player1.get_sum(card1, card2)
         sum2 = player2.get_sum(card3, card4)
@@ -213,7 +234,7 @@ class CardGameGUI:
             result = f"{card1}和{card2}之和等于{card3}和{card4}之和"
 
         messagebox.showinfo("比较结果", result)
-        
+
         match self.flag:
             case 1:
                 self.setup_game_screen_p2()
